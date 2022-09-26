2e23091f8727737c8d3e177c2e30be02125cb0fb
Josh Grant <joshuajamesgrant02@gmail.com>
2022-09-26 00:50:00 -0500

    began a discussion "timer"

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import React from 'react';
 function App() {
   const [currentDay, setCurrentDay] = React.useState(0);
   const [isDaytime, setIsDaytime] = React.useState(false);
+  const [discussionTimer, setDiscussionTimer] = React.useState(60);
+
+
   const dayOrNightString = isDaytime ? "Day" : "Night"
   const dayOrNightOppositeString = isDaytime ? "Night" : "Day"
 
@@ -12,8 +15,10 @@ function App() {
     setIsDaytime(false);
   }
 
-  const transitionDay = () => {
-    if (!isDaytime) {
+  const transitionDayNight = () => {
+    if (isDaytime) {
+      setDiscussionTimer(60);
+    } else {
       setCurrentDay(currentDay + 1);
     }
     setIsDaytime(!isDaytime)
@@ -22,13 +27,17 @@ function App() {
   return (
     <div className="App">
       <h3>Welcome</h3>
-      <button onClick={restartGame}>{"Restart Game"}</button>
-
-      <button onClick={transitionDay}>{"Move To " + dayOrNightOppositeString}</button>
-
-      <label>{"It is " + dayOrNightString + "time of day #" + currentDay}</label>
-      
-
+      <div>
+        <button onClick={restartGame}>{"Restart Game"}</button>
+        <button onClick={transitionDayNight}>{"Move To " + dayOrNightOppositeString}</button>
+        <label>{"It is " + dayOrNightString + "time of day #" + currentDay}</label>
+      </div>
+      {isDaytime &&
+        <div>
+          <button onClick={() => setDiscussionTimer(discussionTimer-1)}>{"Begin discussion timer"}</button>
+          <label>{"There are " + discussionTimer + " seconds left"}</label>
+        </div>
+      }
     </div>
   );
 }
